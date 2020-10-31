08b2b26ad516b63c3a27e91641fff4ecfa1fffe0
testsoso <tnt.scc@gmail.com>
2020-10-31 09:19:20 +0800

    返回上级，返回首页，样式修改

--- webapp/src/components/Bookdetalis/index.jsx
+++ webapp/src/components/Bookdetalis/index.jsx
@@ -1,15 +1,28 @@
-import React from 'react';
+import React,{Component} from 'react';
 import { NavBar, Icon } from 'antd-mobile';
 import { Accordion, List } from 'antd-mobile';
 import { BackTop } from 'antd';
 
 import { Collapse } from 'antd';
+import 'whatwg-fetch';
+
+
+import Home from '../Home/index';
+import Login from '../Login/index';
 
 import 'antd-mobile/dist/antd-mobile.css';
 // import './bookdetalis.css';
 import './style.scss'
 
+// import '../../json/书本数据.json'
 import { LeftOutlined, HomeOutlined, SearchOutlined } from '@ant-design/icons';
+
+
+import creatHistory from 'history/createBrowserHistory' 
+
+const history = creatHistory();
+
+
 
 
 // class AccordionExmple extends React.Component
@@ -17,6 +30,19 @@ import { LeftOutlined, HomeOutlined, SearchOutlined } from '@ant-design/icons';
 class Bookdetalis extends React.Component {
 
 
+    constructor(props){
+        super(props)
+        this.state={
+           datas:[]
+         }
+        this.getData=this.getData.bind(this)
+  }
+  getData(){
+       fetch('../../json/书本数据.json')
+       .then(response=>response.json())
+       .then(data=>{this.setState({datas:data.data})})
+       .catch(e=>{console.log("error")})
+  }
 
 
 
@@ -24,22 +50,60 @@ class Bookdetalis extends React.Component {
     onChange = (key) => {
         console.log(key);
     }
+
+
+    menu = [
+        {
+            id: 1,
+            title: '首页',
+            path: '/home',
+            name: 'home',
+            component: Home,
+        },
+        
+     ]
+  
+     data = this.menu.map((item, index) => ({
+        path: item.path,
+     }));
+
+    // 返回上一页功能
+     btnFn=()=>{
+        history.goBack();
+     }
+
+    // 返回首页的功能
+     home_index(){
+         this.props.history.push('/home')
+     }
+
     render() {
         return (
+
+            
             <div className="bookdedalisbox" >
+                
+                <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"></meta>
                 {/* Bookdetalis哈哈哈哈哈 */}
                 < NavBar
+                    datas={console.log(this.getData,111)}
+                    
+                    
                     className='top-navbar'
                     mode="dark"
                     icon={< Icon type="left" />}
-                    onLeftClick={() => console.log('onLeftClick')}
+                    // 点击跳转上一页
+                    onLeftClick={this.btnFn}
                     rightContent={
                         [
                             // <Icon key="0" type="search" style={{ marginRight: '16px' }} />,
 
                             <div className="icons-list"
                                 key='1'>
-                                <HomeOutlined />
+                                <HomeOutlined 
+                                // 点击跳转回首页
+                                onClick={this.home_index.bind(this)}
+                                />
                             </div>
 
                         ]}
