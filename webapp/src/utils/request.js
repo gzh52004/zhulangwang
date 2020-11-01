394ec708118fea99880079a0cf6be20ce97dd7c1
lanamo <410108462@qq.com>
2020-11-01 19:27:52 +0800

    reg login

--- webapp/src/utils/request.js
+++ webapp/src/utils/request.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 let baseUrl = 'http://47.98.51.123:2000'
 
 let request = axios.create({
-   baseURL:baseUrl + '/user',
+   baseURL:baseUrl + '/'
 })
 
 export default request;
